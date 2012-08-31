2d68563a703aaf8e68bb172d4e5ce23dcedcac07
SENDA Keijiro <senda.keijiro@gmail.com>
2012-08-31 11:56:32 +0900

    Added logout.php to logout correctly.

---
+++ logout.php
@@ -0,0 +1,20 @@
+<?php
+// Sample code of Facebook PHP SDK said "to logout, use '$facebook->getLogoutUrl();'".
+// But this code is not enough for remaining sessions.
+// To logout correctly, use BaseFacebook::destroySession() method.
+//
+// <a href="http://d.hatena.ne.jp/imaiworks/20120518/1337310167">facebook php sdk api でログアウトする方法 - いろいろつまみ食い</a>
+
+require_once 'bootstrap.php';
+
+$facebook = new Facebook(array(
+  'appId'  => Config::$appId,
+  'secret' => Config::$secret,
+));
+
+$facebook->getLogoutUrl(array(
+    'next' => $_SERVER['HTTP_REFERER']
+));
+$facebook->destroySession();
+
+header("Location: {$logoutUrl}");
---
+++ sample2.php
@@ -0,0 +1,64 @@
+<?php
+require_once 'bootstrap.php';
+
+$facebook = new Facebook(array(
+  'appId'  => Config::$appId,
+  'secret' => Config::$secret,
+));
+
+$user = $facebook->getUser();
+
+if ($user) {
+  try {
+    $user_profile = $facebook->api('/me');
+    $customs = $facebook->api('/me?fields=id,name,favorite_athletes,about,albums');
+  } catch (FacebookApiException $e) {
+    error_log($e);
+    $user = null;
+  }
+}
+
+if ($user) {
+  $logoutUrl = 'logout.php';
+} else {
+  $loginUrl = $facebook->getLoginUrl(array(
+    'scope' => 'user_photos,user_likes',
+  ));
+}
+?>
+<!doctype html>
+<html xmlns:fb="http://www.facebook.com/2008/fbml">
+  <head>
+    <title>PHP SDK Sample 2</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
+    <h1>PHP SDK Sample 2</h1>
+
+    <?php if ($user): ?>
+      <a href="<?php echo $logoutUrl; ?>">Logout</a>
+    <?php else: ?>
+      <div>
+        Login using OAuth 2.0 handled by the PHP SDK:
+        <a href="<?php echo $loginUrl; ?>">Login with Facebook</a>
+      </div>
+    <?php endif ?>
+
+    <h3>$user</h3>
+    <pre><?php var_dump($user); ?></pre>
+
+    <h3>PHP Session</h3>
+    <pre><?php print_r($_SESSION); ?></pre>
+
+    <?php if ($user): ?>
+      <h3>You</h3>
+      <img src="https://graph.facebook.com/<?php echo $user; ?>/picture">
+
+      <h3>Your User Object (/me)</h3>
+      <pre><?php print_r($user_profile); ?></pre>
+    <?php else: ?>
+      <strong><em>You are not Connected.</em></strong>
+    <?php endif ?>
+
+  </body>
+</html>
